d211383ae516eb3db629743e316f9b8e8b260508
Ved2604 <59091710+Ved2604@users.noreply.github.com>
2024-02-02 18:28:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import  { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA( {manifest: { 
+      "name": "Vedio call application",
+      "short_name": "Ve_call",
+      "description": "This is an app to for vedio calling between two peers.",
+      "start_url": "/",
+      "display": "standalone",
+      "background_color": "#121212",
+      "theme_color": "#4CAF50",
+      "icons": [
+          { 
+            "src":"maskable_icon.png",
+            "sizes":"240x240",
+            "type": "image/png",
+            "purpose": "any maskable"
+          },
+        { 
+          "src": "logo512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        },
+        {
+          "src": "logo384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "logo256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        },
+        {
+          "src": "logo192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        }
+      ]
+    }
+  }
+    )
+  ],
 })
